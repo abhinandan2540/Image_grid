73d56447eec8381868d44ffc1c08cb1b73b1a159
Abhinandan Mandal <105782385+abhinandan2540@users.noreply.github.com>
2024-11-04 16:26:07 +0530

    Add files via upload

---
+++ IGL.js
@@ -0,0 +1,28 @@
+let Lama = document.getElementById("lama");
+let Nau = document.getElementById("nau");
+let Haloo = document.getElementById("nau1");
+let Coup = document.getElementById("lama1");
+let City = document.getElementById("lama2");
+let Book = document.getElementById("nau2");
+
+
+// in the JS the window.open() is used for opeing a new page by clicking
+Lama.addEventListener("click", () => {
+    window.open("https://www.youtube.com/watch?v=X0bE86akgmw", "_blank");
+});
+
+Nau.addEventListener("click", () => {
+    window.open("https://www.youtube.com/watch?v=Xr8XzEGjLvo", "_blank");
+});
+Haloo.addEventListener("click", () => {
+    window.open("https://www.youtube.com/watch?v=R-VRAemIvbI", "_blank");
+});
+Coup.addEventListener("click", () => {
+    window.open("https://www.youtube.com/watch?v=ad0DGhPFLr4", "_blank");
+});
+City.addEventListener("click", () => {
+    window.open("https://www.youtube.com/watch?v=d_BxBOIFQP4", "_blank");
+});
+Book.addEventListener("click", () => {
+    window.open("https://www.youtube.com/watch?v=xW3WaQzxE14", "_blank");
+});
